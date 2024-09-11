24aae90284ac2efeff043950225bbab3a411108a
hardstylewyl <827778732@qq.com>
2024-09-11 23:44:21 +0800

    add: 添加OpenIddictScopeManager并提供scope按name批量获取

--- src/IdentityServer.Infrastructure/OpenIddict/OpenIddictApplicationManager.cs
+++ src/IdentityServer.Infrastructure/OpenIddict/OpenIddictApplicationManager.cs
@@ -17,8 +17,29 @@ public sealed class OpenIddictApplicationManager(
 	public Task<List<OpenIddictEntityFrameworkCoreApplication>> ListByClientIdAsync(IEnumerable<string> clientIds,
 		CancellationToken cancellationToken = default)
 	{
-		return this.ListAsync(x => 
-			x.Where(y => clientIds.Contains(y.ClientId)), cancellationToken)
+		return this.ListAsync(x =>
+				x.Where(y => clientIds.Contains(y.ClientId)), cancellationToken)
 			.ToListAsync();
+	}
+
+	public async Task<OpenIddictEntityFrameworkCoreApplication> ResourceCreateAsync(
+		string clientId,
+		string clientSecret,
+		string displayName,
+		CancellationToken cancellationToken = default)
+	{
+		var descriptor = new OpenIddictApplicationDescriptor
+		{
+			ClientId = clientId,
+			ClientSecret = clientSecret,
+			DisplayName = displayName,
+			ClientType = OpenIddictConstants.ClientTypes.Confidential,
+			Permissions =
+			{
+				OpenIddictConstants.Permissions.Endpoints.Introspection
+			}
+		};
+		
+		return await CreateAsync(descriptor, cancellationToken);
 	}
 }
---
+++ src/IdentityServer.Infrastructure/OpenIddict/OpenIddictScopeManager.cs
@@ -0,0 +1,24 @@
+﻿using IdentityServer.CrossCuttingConcerns.Extensions;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using OpenIddict.Abstractions;
+using OpenIddict.Core;
+using OpenIddict.EntityFrameworkCore.Models;
+
+namespace IdentityServer.Infrastructure.OpenIddict;
+
+public sealed class OpenIddictScopeManager(
+	IOpenIddictScopeCache<OpenIddictEntityFrameworkCoreScope> cache,
+	ILogger<OpenIddictScopeManager<OpenIddictEntityFrameworkCoreScope>> logger,
+	IOptionsMonitor<OpenIddictCoreOptions> options,
+	IOpenIddictScopeStoreResolver resolver)
+	: OpenIddictScopeManager<OpenIddictEntityFrameworkCoreScope>(cache, logger, options, resolver)
+{
+	public Task<List<OpenIddictEntityFrameworkCoreScope>> ListByNameAsync(IEnumerable<string> scopeNames,
+		CancellationToken cancellationToken = default)
+	{
+		return this.ListAsync(x =>
+				x.Where(y => scopeNames.Contains(y.Name)), cancellationToken)
+			.ToListAsync();
+	}
+}
